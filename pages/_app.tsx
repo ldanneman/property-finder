56f654105250aa6db6c5b3cd403c33ff64038d49
Lev <levdanneman@gmail.com>
2022-04-09 16:38:04 +0300

    apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,21 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/index.css';
 import { AuthProvider } from 'src/auth/useAuth';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from 'src/apollo';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const client = useApollo();
+
   return (
     <AuthProvider>
-      <Head>
-        <title>Property Finder</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />;
+      <ApolloProvider client={client}>
+        <Head>
+          <title>Property Finder</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />;
+      </ApolloProvider>
     </AuthProvider>
   );
 }
