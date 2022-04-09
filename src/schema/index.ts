88dc1fd27a5852e2e0c4f59e4585f90e2656bdf0
Lev <levdanneman@gmail.com>
2022-04-09 13:38:43 +0300

    gql initial

---
+++ src/schema/index.ts
@@ -0,0 +1,17 @@
+import { buildSchemaSync, Resolver, Query } from 'type-graphql';
+// import { ImageResolver } from './image';
+// import { HouseResolver } from './house';
+import { authChecker } from './auth';
+
+class DummyResolver {
+  @Query((_returns) => String)
+  hello() {
+    return 'Hello World!';
+  }
+}
+
+export const schema = buildSchemaSync({
+  resolvers: [DummyResolver],
+  emitSchemaFile: process.env.NODE_ENV === 'development',
+  authChecker,
+});
